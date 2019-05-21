710decc59c5f92ee17b6ead2fcce8b118e2150d9
Devin Kelly-Collins <dkellycollins@gmail.com>
2019-05-20 22:04:18 -0500

    Cleaned up Demo.

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,13 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { TextField, Paper, MenuItem, Grid, Typography } from '@material-ui/core';
 
-/**
- * The is a bare minimum example of managing state in react. The goal is to update this class to be
- * a functional component with "useState"
- */
-
 function Demo() {
-
+  return null;
 }
 
 export default Demo;
